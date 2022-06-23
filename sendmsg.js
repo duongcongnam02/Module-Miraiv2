dbd39fcb8f35a5d04e4d3e3c4f78998b73d797f2
Nam <107612309+duongcongnam5@users.noreply.github.com>
2022-06-23 13:36:03 +0700

    Update sendmsg.js

--- sendmsg.js
+++ sendmsg.js
@@ -1,3 +1,7 @@
+const moment = require("moment-timezone")
+const request = require("request")
+const imageDownload = require("image-downloader")
+const fse = require("fs-extra")
 module.exports.config = {
   name: "sendmsg",
   version: "1.1.1",
@@ -11,9 +15,10 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args, Users }) {
   var url = event.type == "message_reply" ? event.messageReply.attachments[0].url : event.attachments.length != 0 ? event.attachments[0].url : "nit"
   const id = parseInt(args[0])
-  const content = args.splice(1).join(" ") || `chỉ có ảnh đến bạn`
+  const content = args.splice(1).join(" ") || `chỉ có file đến bạn`
   let dataUID = await Users.getData(event.senderID) || {}
-  var msg = `📧 Tin nhắn từ Admin ${dataUID.name || ""}\n» Nội dung: ${content}`
+  const fullTime = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss || DD/MM/YYYY")
+  var msg = `📧 Tin nhắn từ Admin ${dataUID.name || ""}\n⏱ Time: ${fullTime}\n» Nội dung: ${content}`
   if (url != "nit") {
     d = await D(url), uwu = {
       body: msg,
@@ -23,10 +28,12 @@ module.exports.run = async function({ api, event, args, Users }) {
   await api.sendMessage(uwu, id, async (error, info) => {
     if (error) return api.sendMessage(`» Đã xảy ra lỗi khi gửi tin đến ID: ${id}`, event.threadID, event.messageID)
     else api.sendMessage(`📨 Đã gửi tin đến ID thành công`, event.threadID, event.messageID)
-    if (url != "nit") require("fs-extra").unlinkSync(d.path)
+    if (url != "nit") fse.unlinkSync(d.path)
     return global.client.handleReply.push({
       name: this.config.name,
       messageID: info.messageID,
+      msgId: event.type == "message_reply" ? event.messageID : event.messageID,
+      nameAdmin: dataUID.name,
       id: event.threadID
     })
   })
@@ -34,8 +41,9 @@ module.exports.run = async function({ api, event, args, Users }) {
 module.exports.handleReply = async function({ api, event, handleReply: h, Users, Threads }) {
   let dataUID = await Users.getData(event.senderID) || {}
   let dataTID = await Threads.getData(event.threadID) || {}
+  const fullTime = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss || DD/MM/YYYY")
   if (event.senderID != api.getCurrentUserID()) {
-    var msg = global.config.ADMINBOT.includes(event.senderID) ? `📩 Phản hồi từ Admin ${dataUID.name || ""}\n» Nội dung: ${event.attachments.length == 0 ? event.body : "chỉ có ảnh đến bạn"}` : `📩 Phản hồi từ User ${dataUID.name || ""}${event.isGroup == true ? `🛡 Nhóm ${dataTID.threadInfo.threadName}` : ``}\n» Nội dung: ${event.attachments.length == 0 ? event.body : "chỉ có ảnh đến bạn"}`
+    var msg = global.config.ADMINBOT.includes(event.senderID) == true ? `📩 Phản hồi từ Admin ${dataUID.name || ""}\n⏱ Time: ${fullTime}\n» Nội dung: ${event.attachments.length == 0 ? event.body : "chỉ có file đến bạn"}` : `📩 Phản hồi từ User ${dataUID.name || ""}${event.isGroup == true ? `\n🛡 Nhóm ${dataTID.threadInfo.threadName || `noname`}` : ``}\n⏱ Time: ${fullTime}\n» Nội dung: ${event.attachments.length == 0 ? event.body : "chỉ có file đến bạn"}`
     if (event.attachments.length != 0) {
       d = await D(event.attachments[0].url), uwu = {
         body: msg,
@@ -44,26 +52,29 @@ module.exports.handleReply = async function({ api, event, handleReply: h, Users,
     } else uwu = msg
     await api.sendMessage(uwu, h.id, async (error, info) => {
       if (error) return api.sendMessage(`» Đã xảy ra lỗi khi phản hồi đến ID: ${h.id}`, event.threadID, event.messageID)
-      else api.sendMessage(`📨 Đã phản hồi đến ${global.config.ADMINBOT.includes(event.senderID) ? `Admin` : `User`} ${dataUID.name || ""}`, event.threadID, event.messageID)
-      if (event.attachments.length != 0) require("fs-extra").unlinkSync(d.path)
+      else api.sendMessage(`📨 Đã phản hồi đến ${global.config.ADMINBOT.includes(event.senderID) == true ? `User ${h.nameUser}` : `Admin ${h.nameAdmin}`}`, event.threadID, event.messageID)
+      if (event.attachments.length != 0) fse.unlinkSync(d.path)
       return global.client.handleReply.push({
         name: this.config.name,
         messageID: info.messageID,
+        msgId: event.type == "message_reply" ? event.messageID : event.messageID,
+        nameAdmin: h.nameAdmin,
+        nameUser: global.config.ADMINBOT.includes(event.senderID) == true ? `` : dataUID.name,
         id: event.threadID
       })
-    })
+    }, h.msgId)
   } else return;
 }
 async function D(link) {
-  const get = require("request").get(link)
+  const get = request.get(link)
   const namefile = get.uri.pathname;
   const path = __dirname + "/cache/" + namefile.slice(namefile.lastIndexOf("/") + 1)
-  await require("image-downloader").image({
+  await imageDownload.image({
     url: link,
     dest: path
   })
   return {
-    images: require("fs-extra").createReadStream(path),
+    images: fse.createReadStream(path),
     path
   }
 }
